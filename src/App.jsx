a1471c04009aef8d66a22bac3b01a37282a11800
Esteban Martinez <camilo.martinezmma@cun.edu.co>
2023-11-25 22:11:54 -0500

    v1.1.1 se corrige encadenamiento opcional se cambia a objeto para cargar imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,6 @@ import es from "i18n-iso-countries/langs/es.json";
 
 countries.registerLocale(es);
 
-function getBackgroundImage(icon) {
-  return `bg-[url(/bagraunds/${icon}.jpg)]`;
-}
-
 function App() {
   const [climate, setClimate] = useState(null);
 
@@ -33,15 +29,37 @@ function App() {
       });
   };
 
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(success);
   }, []);
 
+  const bagraunds = {
+    '01d': 'bg-[url(/bagraunds/01d.jpg)]',
+    '01n': 'bg-[url(/bagraunds/01n.jpg)]',
+    '02d': 'bg-[url(/bagraunds/02d.jpg)]',
+    '02n': 'bg-[url(/bagraunds/02n.jpg)]',
+    '03d': 'bg-[url(/bagraunds/03d.jpg)]',
+    '03n': 'bg-[url(/bagraunds/03n.jpg)]',
+    '04d': 'bg-[url(/bagraunds/04d.jpg)]',
+    '04n': 'bg-[url(/bagraunds/04n.jpg)]',
+    '09d': 'bg-[url(/bagraunds/09d.jpg)]',
+    '09n': 'bg-[url(/bagraunds/09n.jpg)]',
+    '10d': 'bg-[url(/bagraunds/10d.jpg)]',
+    '10n': 'bg-[url(/bagraunds/10n.jpg)]',
+    '11d': 'bg-[url(/bagraunds/11d.jpg)]',
+    '11n': 'bg-[url(/bagraunds/11n.jpg)]',
+    '13d': 'bg-[url(/bagraunds/13d.jpg)]',
+    '13n': 'bg-[url(/bagraunds/13n.jpg)]',
+    '50d': 'bg-[url(/bagraunds/50d.jpg)]',
+    '50n': 'bg-[url(/bagraunds/50n.jpg)]',
+
+  }
+  
+
   return (
     <main
-      className={` text-white h-screen flex items-center justify-center ${
-        climate ? getBackgroundImage(climate?.weather[0].icon) : ""
-      } bg-cover bg-center`}
+      className={` text-white h-screen flex items-center justify-center ${bagraunds[climate?.weather[0].icon]} bg-cover bg-center`}
     >
       {climate ? <WeatherInformation climate={climate} /> : "...cargando"}
     </main>
